d1f6e9881d0ec3fe893b59e4f82ac497068574ec
Ak-Srivastav <amit.edu@outlook.com>
2024-07-16 02:31:58 +0530

    fixed redis host

--- server/config/redis.js
+++ server/config/redis.js
@@ -1,6 +1,8 @@
 import Redis from "ioredis";
 import { REDIS_HOST, REDIS_PASSWORD, REDIS_PORT, REDIS_USERNAME } from "./index.js";
-let redis;
+
+// for dev
+// let redis;
 // try {
 //   redis = new Redis({
 //     port: Number(REDIS_PORT), // Redis port
@@ -13,17 +15,14 @@ let redis;
 //   console.log("err", err);
 // }
 
-try {
-  redis = new Redis({
-    port: REDIS_PORT, // Redis port
-    host: REDIS_HOST, // Redis host
-    username: REDIS_USERNAME, // needs Redis >= 6
-    password: REDIS_PASSWORD,
-    // db: 0, // Defaults to 0
-  });
-  // await redis.connect(() => console.log('Redis Connected'));
-} catch (err) {
-  console.log("err", err);
-}
+import createClient from 'ioredis';
+
+const redis = new createClient({
+  password: REDIS_PASSWORD,
+  socket: {
+    host: REDIS_HOST,
+    port: REDIS_PORT
+  }
+});
 
 export default redis;
